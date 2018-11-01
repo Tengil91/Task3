9361106f629fc8e0e0efcb39b2b3e3ed5113f181
Niklas Silfverhielm <kachiun91@gmail.com>
2018-11-01 09:30:58 +0100

    tal mellan 0 och 170 på uppgift 6

--- uppgift6/uppgift6.js
+++ uppgift6/uppgift6.js
@@ -1,4 +1,4 @@
-//Programmet ber användaren att mata in ett tal via prompt. Fakulteten returneras via alert.
+//Programmet ber användaren att mata in ett tal via prompt. Fakulteten returneras via write..
 
 function fakultet(tal1){
     let tal2=1;
@@ -7,6 +7,10 @@ function fakultet(tal1){
     }
     return tal2;
 }
-let n = +prompt("Programmet räknar ut fakulteten av det inmatade numret");
-document.write(`${n}! = ${fakultet(n)}`);
+let n = Math.floor(+prompt("Programmet räknar ut fakulteten av det inmatade numret (som är mindre än 170"));
+if(n>=171||n<0){
+    document.write(":(");
+} else {
+    document.write(`${n}! = ${fakultet(n)}`);
+}
 
